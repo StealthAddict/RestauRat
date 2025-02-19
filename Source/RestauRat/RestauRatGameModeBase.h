c52680f8e35556e189dc87f2fc987c5eccc402eb
StealthAddict <iaruepprich@gmail.com>
2025-02-19 10:52:27 -0600

    Init commit

---
+++ Source/RestauRat/RestauRatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RestauRatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RESTAURAT_API ARestauRatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
